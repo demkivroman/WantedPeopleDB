52bf8ad302058f48f912a248f5316355c7b457f1
demkivroman <demkivroman5@gmail.com>
2018-05-07 21:15:34 +0300

    Add and run home page

--- wantedDB/urls.py
+++ wantedDB/urls.py
@@ -13,9 +13,12 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
+from peoplesWDB.views import basepage
+
 from django.conf.urls import url
 from django.contrib import admin
 
 urlpatterns = [
+    url(r'^', basepage.base ,name = 'home'),
     url(r'^admin/', admin.site.urls),
 ]
