5a765f9db892b43f3de787699203360055f35478
demkivroman <demkivroman5@gmail.com>
2018-07-04 20:16:47 +0300

    Add javascript code for resize input comment field

--- peoplesWDB/static/js/main.js
+++ peoplesWDB/static/js/main.js
@@ -1,8 +1,21 @@
+var ROWS = 1;
 
+function formAutoResize(textBox){
+textBox.addEventListener("scroll", AddRows);
+    if(textBox.value.length == 15){
+        $("#comment_field").attr("rows","1");
+        ROWS = 1;
+     }
+}
+
+function AddRows(){
+ROWS+=2;
+$("#comment_field").attr("rows",ROWS);
+}
 
 
 
 
 $(document).ready(function(){
-
+//formAutoResize();
 });
